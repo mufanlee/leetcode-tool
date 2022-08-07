f99b9ddae2c891a910f8985727264f85791cd3c3
lipeng <344043379@qq.com>
2022-08-07 22:08:37 +0800

    20220805每日一题

---
+++ coding/src/main/java/daily/dd20220805/MainClass.java
@@ -0,0 +1,180 @@
+package daily.dd20220805;
+
+import base.mo.TreeNode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // BFS
+    public TreeNode addOneRow1(TreeNode root, int val, int depth) {
+        if (depth == 1) {
+            return new TreeNode(val, root, null);
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        int level = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+                if (level == depth - 1) {
+                    TreeNode x = new TreeNode(val);
+                    TreeNode y = new TreeNode(val);
+                    x.left = node.left;
+                    y.right = node.right;
+                    node.left = x;
+                    node.right = y;
+                } else {
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+            }
+            level++;
+        }
+        return root;
+    }
+
+    // DFS
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (depth == 1) {
+            return new TreeNode(val, root, null);
+        }
+        dfs(root, 1, val, depth);
+        return root;
+    }
+
+    private void dfs(TreeNode root, int depth, int val, int target) {
+        if (root == null) {
+            return;
+        }
+
+        if (depth == target - 1) {
+            TreeNode x = new TreeNode(val);
+            TreeNode y = new TreeNode(val);
+            x.left = root.left;
+            y.right = root.right;
+            root.left = x;
+            root.right = y;
+        } else {
+            dfs(root.left, depth + 1, val, target);
+            dfs(root.right, depth + 1, val, target);
+        }
+    }
+}
+/*[4,2,6,3,1,5]
+1
+2
+[4,2,null,3,1]
+1
+3*/
+
+public class MainClass {
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+    
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+    
+        int index = 1;
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+    
+            if (index == parts.length) {
+                break;
+            }
+    
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+    
+            if (index == parts.length) {
+                break;
+            }
+    
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+    
+    public static String treeNodeToString(TreeNode root) {
+        if (root == null) {
+            return "[]";
+        }
+    
+        String output = "";
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+    
+            if (node == null) {
+              output += "null, ";
+              continue;
+            }
+    
+            output += String.valueOf(node.val) + ", ";
+            nodeQueue.add(node.left);
+            nodeQueue.add(node.right);
+        }
+        return "[" + output.substring(0, output.length() - 2) + "]";
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            TreeNode root = stringToTreeNode(line);
+            line = in.readLine();
+            int val = Integer.parseInt(line);
+            line = in.readLine();
+            int depth = Integer.parseInt(line);
+            
+            TreeNode ret = new Solution().addOneRow(root, val, depth);
+            
+            String out = treeNodeToString(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
