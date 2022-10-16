e31c9ab97996c192d6f930b3e73a2c7d432ffb16
lipeng <344043379@qq.com>
2022-10-16 17:01:59 +0800

    dd20221012

---
+++ coding/src/main/java/daily/dd20221012/MainClass.java
@@ -0,0 +1,91 @@
+package daily.dd20221012;
+
+import base.mo.ListNode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int numComponents(ListNode head, int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int x : nums) set.add(x);
+        int ans = 0, pre = -1;
+        while (head != null) {
+            if (set.contains(head.val) && !set.contains(pre)) {
+                ans++;
+            }
+            pre = head.val;
+            head = head.next;
+        }
+        return ans;
+    }
+}
+/*[0,1,2,3]
+[0,1,3]
+[0,1,2,3,4]
+[0,3,1,4]*/
+
+public class MainClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+          return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static ListNode stringToListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = stringToIntegerArray(input);
+
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for(int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            ListNode head = stringToListNode(line);
+            line = in.readLine();
+            int[] nums = stringToIntegerArray(line);
+
+            int ret = new Solution().numComponents(head, nums);
+
+            String out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
+}
