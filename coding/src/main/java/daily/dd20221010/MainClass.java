c46d0fa04aff6e1300ebbcb209c21e69b40d4b20
lipeng <344043379@qq.com>
2022-10-16 18:30:43 +0800

    dd20221010

---
+++ coding/src/main/java/daily/dd20221010/MainClass.java
@@ -0,0 +1,65 @@
+package daily.dd20221010;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution {
+    public int minSwap(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int[][] dp = new int[n][2];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = dp[i][1] = n + 10;
+        }
+        dp[0][1] = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums1[i] > nums1[i - 1] && nums2[i] > nums2[i - 1]) {
+                dp[i][0] = dp[i - 1][0];
+                dp[i][1] = dp[i - 1][1] + 1;
+            }
+            if (nums1[i] > nums2[i - 1] && nums2[i] > nums1[i - 1]) {
+                dp[i][0] = Math.min(dp[i][0], dp[i - 1][1]);
+                dp[i][1] = Math.min(dp[i][1], dp[i - 1][0] + 1);
+            }
+        }
+        return Math.min(dp[n - 1][0], dp[n - 1][1]);
+    }
+}
+/*[1,3,5,4]
+[1,2,3,7]
+[0,3,5,8,9]
+[2,1,4,6,9]*/
+
+public class MainClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for (int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[] nums1 = stringToIntegerArray(line);
+            line = in.readLine();
+            int[] nums2 = stringToIntegerArray(line);
+
+            int ret = new Solution().minSwap(nums1, nums2);
+
+            String out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
+}
