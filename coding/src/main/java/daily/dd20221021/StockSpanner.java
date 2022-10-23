463f2ece877d20da5e3610779dbc24d4715a2ff7
lipeng <344043379@qq.com>
2022-10-23 16:15:13 +0800

    dd20221021

---
+++ coding/src/main/java/daily/dd20221021/StockSpanner.java
@@ -0,0 +1,35 @@
+package daily.dd20221021;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class StockSpanner {
+    private Stack<Integer> stack;
+
+    private List<Integer> arr;
+
+    public StockSpanner() {
+        stack = new Stack<>();
+        arr = new ArrayList<>();
+    }
+
+    public int next(int price) {
+        while (!stack.isEmpty() && arr.get(stack.peek()) <= price) {
+            stack.pop();
+        }
+
+        int index = !stack.isEmpty() ? stack.peek() + 1 : 0;
+        arr.add(price);
+        stack.push(arr.size() - 1);
+        return arr.size() - index;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
+/*["StockSpanner","next","next","next","next","next","next","next"]
+[[],[100],[80],[60],[70],[60],[75],[85]]*/
