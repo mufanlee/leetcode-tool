2dfbce297b133d79c852ae35daa0dd5df555bd82
lipeng <344043379@qq.com>
2022-11-28 23:28:00 +0800

    dd20221127

---
+++ coding/src/main/java/daily/dd20221127/MainClass.java
@@ -0,0 +1,60 @@
+package daily.dd20221127;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution {
+    public boolean check(int[] nums) {
+        boolean split = nums[0] >= nums[nums.length - 1];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                if (split) {
+                    split = false;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+/*[3,4,5,1,2]
+[2,1,3,4]
+[1,2,3]*/
+
+public class MainClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+          return new int[0];
+        }
+    
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+    
+    public static String booleanToString(boolean input) {
+        return input ? "True" : "False";
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[] nums = stringToIntegerArray(line);
+            
+            boolean ret = new Solution().check(nums);
+            
+            String out = booleanToString(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
