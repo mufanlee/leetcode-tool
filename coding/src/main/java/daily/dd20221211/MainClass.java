d155adadc38ffd2c0c15849567c57e9abdbdc2fd
lipeng <344043379@qq.com>
2022-12-11 18:24:19 +0800

    dd20221211

---
+++ coding/src/main/java/daily/dd20221211/MainClass.java
@@ -0,0 +1,53 @@
+package daily.dd20221211;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution {
+    public int minOperations(int[] nums) {
+        int ans = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] <= nums[i - 1]) {
+                ans += nums[i - 1] + 1 - nums[i];
+                nums[i] = nums[i - 1] + 1;
+            }
+        }
+        return ans;
+    }
+}
+/*[1,1,1]
+[1,5,2,4,1]
+[8]*/
+
+public class MainClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for (int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[] nums = stringToIntegerArray(line);
+
+            int ret = new Solution().minOperations(nums);
+
+            String out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
+}
