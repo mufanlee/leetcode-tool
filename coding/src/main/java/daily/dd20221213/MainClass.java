ee1498c684426dcb76bb367a9838c0afbbe64078
lipeng <344043379@qq.com>
2022-12-13 22:26:46 +0800

    dd20221213

---
+++ coding/src/main/java/daily/dd20221213/MainClass.java
@@ -0,0 +1,43 @@
+package daily.dd20221213;
+
+import base.json.JsonArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        int mask = 0;
+        for (char c : sentence.toCharArray()) {
+            mask |= 1 << (c - 'a');
+        }
+        return mask == (1 << 26) - 1;
+    }
+}
+/*"thequickbrownfoxjumpsoverthelazydog"
+"leetcode"*/
+
+public class MainClass {
+    public static String stringToString(String input) {
+        return JsonArray.readFrom("[" + input + "]").get(0).asString();
+    }
+
+    public static String booleanToString(boolean input) {
+        return input ? "True" : "False";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            String sentence = stringToString(line);
+
+            boolean ret = new Solution().checkIfPangram(sentence);
+
+            String out = booleanToString(ret);
+
+            System.out.print(out);
+        }
+    }
+}
