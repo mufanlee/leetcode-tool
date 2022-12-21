e7b90f16a83b08785bd0fe77c78a8a839236fb8c
lipeng <344043379@qq.com>
2022-12-21 22:38:21 +0800

    dd20221208

---
+++ coding/src/main/java/daily/dd20221208/MainClass.java
@@ -0,0 +1,42 @@
+package daily.dd20221208;
+
+import base.json.JsonArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution {
+    public boolean squareIsWhite(String coordinates) {
+        int x = coordinates.charAt(0) - 'a';
+        int y = coordinates.charAt(1) - '1';
+        return (x & 1) != (y & 1);
+    }
+}
+/*"a1"
+"h3"
+"c7"*/
+
+public class MainClass {
+    public static String stringToString(String input) {
+        return JsonArray.readFrom("[" + input + "]").get(0).asString();
+    }
+    
+    public static String booleanToString(boolean input) {
+        return input ? "True" : "False";
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            String coordinates = stringToString(line);
+            
+            boolean ret = new Solution().squareIsWhite(coordinates);
+            
+            String out = booleanToString(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
