e2ca6b80a476ce2f10aa648462c7159ed61f76f0
lipeng <344043379@qq.com>
2022-12-23 22:11:13 +0800

    dd20221223

---
+++ coding/src/main/java/daily/dd20221223/MainClass.java
@@ -0,0 +1,49 @@
+package daily.dd20221223;
+
+import base.json.JsonArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int ans = 0;
+        for (String op : operations) {
+            if ("X++".equals(op) || "++X".equals(op)) {
+                ans++;
+            } else {
+                ans--;
+            }
+        }
+        return ans;
+    }
+}
+/*["--X","X++","X++"]
+["++X","++X","X++"]
+["X++","++X","--X","X--"]*/
+
+public class MainClass {
+    public static String[] stringToStringArray(String line) {
+        JsonArray jsonArray = JsonArray.readFrom(line);
+        String[] arr = new String[jsonArray.size()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = jsonArray.get(i).asString();
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            String[] operations = stringToStringArray(line);
+
+            int ret = new Solution().finalValueAfterOperations(operations);
+
+            String out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
+}
