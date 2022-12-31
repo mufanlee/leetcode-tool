a189f1cc624514d5d34aa81702bd4a6074111f18
lipeng <344043379@qq.com>
2022-12-31 13:46:40 +0800

    dd20221231

---
+++ coding/src/main/java/daily/dd20221231/MainClass.java
@@ -0,0 +1,58 @@
+package daily.dd20221231;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int ans = 0;
+        for (int i = 0; i < seats.length; i++) {
+            ans += Math.abs(seats[i] - students[i]);
+        }
+        return ans;
+    }
+}
+/*[3,1,5]
+[2,7,4]
+[4,1,5,9]
+[1,3,2,6]
+[2,2,6,6]
+[1,3,2,6]*/
+
+public class MainClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+          return new int[0];
+        }
+    
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[] seats = stringToIntegerArray(line);
+            line = in.readLine();
+            int[] students = stringToIntegerArray(line);
+            
+            int ret = new Solution().minMovesToSeat(seats, students);
+            
+            String out = String.valueOf(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
