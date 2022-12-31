84ae673b0c24a68ed0e4f9e89dd16e38beadb114
lipeng <344043379@qq.com>
2022-12-31 21:12:16 +0800

    dd20221202

---
+++ coding/src/main/java/daily/dd20221203/MainClass.java
@@ -0,0 +1,48 @@
+package daily.dd20221203;
+
+import base.json.JsonArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution {
+    public int secondHighest(String s) {
+        int first = -1, second = -1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                int num = c - '0';
+                if (num > first) {
+                    second = first;
+                    first = num;
+                } else if (num < first && num > second) {
+                    second = num;
+                }
+            }
+        }
+        return second;
+    }
+}
+/*"dfa12321afd"
+"abc1111"*/
+
+public class MainClass {
+    public static String stringToString(String input) {
+        return JsonArray.readFrom("[" + input + "]").get(0).asString();
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            String s = stringToString(line);
+            
+            int ret = new Solution().secondHighest(s);
+            
+            String out = String.valueOf(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
