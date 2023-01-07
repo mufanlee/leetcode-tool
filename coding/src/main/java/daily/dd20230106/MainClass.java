9adc9aca8228d5334d04e460c0584a20919d648f
lipeng <344043379@qq.com>
2023-01-07 20:33:10 +0800

    dd20230106

---
+++ coding/src/main/java/daily/dd20230106/MainClass.java
@@ -0,0 +1,40 @@
+package daily.dd20230106;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution {
+    public int countEven(int num) {
+        int ans = 0;
+        for (int i = 2; i <= num; i++) {
+            int sum = 0, j = i;
+            while (j > 0) {
+                sum += j % 10;
+                j /= 10;
+            }
+            if ((sum & 1) == 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
+/*4
+30*/
+
+public class MainClass {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int num = Integer.parseInt(line);
+
+            int ret = new Solution().countEven(num);
+
+            String out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
+}
